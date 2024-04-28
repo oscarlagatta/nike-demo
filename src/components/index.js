e3d813513859c9bc38cc71af034df71c1efb5ea7
Oscar <oscarlagatta@gmail.com>
2024-04-28 18:31:51 +0100

    Add ServiceCard component and update Services section
    Created a new ServiceCard component and integrated it into the Services.jsx section. Services are now rendered dynamically based on the data provided in the "services" array, improving modularity and ease of updates to the services section.

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import Nav from './Nav';
 import Button from './Button'
 import PopularProductCard from "./PopularProductCard";
+import ServiceCard from './ServiceCard';
 
 export {
     Nav,
     Button,
     PopularProductCard,
+    ServiceCard
 }
